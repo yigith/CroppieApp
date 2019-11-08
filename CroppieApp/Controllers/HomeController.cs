44ba81f2a3faf41e3b5b978c7800a7aaa342f57c
Yigit Haciefendioglu <yigith1@gmail.com>
2019-11-08 18:27:54 +0300

    Add project files.

---
+++ CroppieApp/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CroppieApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SaveImage(string imgBase64)
+        {
+            byte[] data = Convert.FromBase64String(imgBase64.Substring(22));
+            string fileName = Guid.NewGuid() + ".jpg";
+            string savePath = Path.Combine(
+                Server.MapPath("~/Upload"), fileName
+                );
+            System.IO.File.WriteAllBytes(savePath, data);
+
+            return RedirectToAction("ShowImage", 
+                new { FileName = fileName });
+        }
+
+        public ActionResult ShowImage(string fileName)
+        {
+            ViewBag.fileName = fileName;
+            return View();
+        }
+    }
+}
